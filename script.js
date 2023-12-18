57b365b90c6b594fa9d0775fcf12a1aef13b63d4
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-18 21:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function volume_sphere() {
   let item = document.getElementById("radius").value;
-  let vol = Math.pow(Number(item),3)* 4/3 * Math.PI;
-  console.log(vol);
-  document.getElementById("volume").value = vol;
+  let volume= Math.pow(Number(item),3)* 4/3 * Math.PI;
+  console.log(volume);
+  document.getElementById("volume").value = volume;
   console.log(document.getElementById("volume"));
 } 
 
